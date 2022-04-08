bbdf79a977a0a4ad7ff3c74795b939b9fdcac724
Cynthia Castillo <ca.castillo@gmail.com>
2022-04-08 12:25:22 -0500

    Registro e Inicio de Sesión

---
+++ src/main/java/com/codingdojo/cynthia/repositorios/UserRepository.java
@@ -0,0 +1,15 @@
+package com.codingdojo.cynthia.repositorios;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.codingdojo.cynthia.modelos.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long>{
+	
+	Optional<User> findByEmail(String email);
+	
+}
---
+++ src/main/java/com/codingdojo/cynthia/servicios/AppService.java
@@ -0,0 +1,76 @@
+package com.codingdojo.cynthia.servicios;
+
+import java.util.Optional;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+
+import com.codingdojo.cynthia.modelos.LoginUser;
+import com.codingdojo.cynthia.modelos.User;
+import com.codingdojo.cynthia.repositorios.UserRepository;
+
+@Service
+public class AppService {
+	
+	@Autowired
+	private UserRepository repositorio_user;
+	
+	public User register(User nuevoUsuario, BindingResult result) {
+		
+		String nuevoEmail = nuevoUsuario.getEmail();
+		
+		//Revisamos si existe el correo electrónico en BD
+		if(repositorio_user.findByEmail(nuevoEmail).isPresent()) {
+			result.rejectValue("email", "Unique", "El correo fue ingresado previamente.");
+		}
+		
+		if(! nuevoUsuario.getPassword().equals(nuevoUsuario.getConfirm()) ) {
+			result.rejectValue("confirm", "Matches", "Las contraseñas no coiniciden");
+		}
+		
+		if(result.hasErrors()) {
+			return null;
+		} else {
+			//Encriptamos contraseña
+			String contra_encr = BCrypt.hashpw(nuevoUsuario.getPassword(), BCrypt.gensalt());
+			nuevoUsuario.setPassword(contra_encr);
+			//Guardo usuario
+			return repositorio_user.save(nuevoUsuario);
+		}
+		
+	}
+	
+	public User login(LoginUser nuevoLogin, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			return null;
+		}
+		
+		//Buscamos por correo
+		Optional<User> posibleUsuario = repositorio_user.findByEmail(nuevoLogin.getEmail());
+		if(!posibleUsuario.isPresent()) {
+			result.rejectValue("email", "Unique", "Correo ingresado no existe");
+			return null;
+		}
+		
+		User user_login = posibleUsuario.get();
+		
+		//Comparamos contraseñas encriptadas
+		if(! BCrypt.checkpw(nuevoLogin.getPassword(), user_login.getPassword()) ) {
+			result.rejectValue("password", "Matches", "Contraseña inválida");
+		}
+		
+		if(result.hasErrors()) {
+			return null;
+		} else {
+			return user_login; 
+		}
+		
+		
+	}
+	
+	
+	
+}
